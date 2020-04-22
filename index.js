026367b1710d4d2b49df177ffdcce8ce80c00f81
Chris Washington <thorough.developer@gmail.com>
2020-04-22 17:26:38 -0400

    ready to go just waiting for a v12 backport before I can pull in

--- index.js
+++ index.js
@@ -1,41 +1,29 @@
 const fp = require('fastify-plugin');
-const createNamespace = require('cls-hooked').createNamespace;
-const hyperid = require('hyperid');
-const generator = hyperid();
-const id = generator();
 
-const namespaceIdentifier = hyperid.decode(id).uuid;
-
-let namespace;
-
-const getDefaults = (key) => {
-  return namespace.get(namespaceIdentifier) || {};
-}
+const { AsyncLocalStorage } = require('async_hooks');
+const asyncLocalStorage = new AsyncLocalStorage();
 
 const getContext = (key) => {
-  let value;
-  if (namespace && namespace.active) {
-   value = namespace.get(key) || getDefaults()[key];
-  }
-  return value;
+  const store = asyncLocalStorage.getStore();
+  
+  return store != null ? store[key] : undefined;  
 };
 
 const setContext = (key, value) => {
-  if (namespace && namespace.active) {
-   namespace.set(key, value);
+  const store = asyncLocalStorage.getStore();
+
+  if (store != null) {
+    store[key] = value;
+    asyncLocalStorage.enterWith(store);
   }
 };
 
 function plugin (fastify, opts, next) {
-  namespace = createNamespace(namespaceIdentifier);
 
   fastify.addHook('onRequest', (req, res, done) => {
-    namespace.run(() => {
-      if (opts && opts.defaults) {
-       setContext(namespaceIdentifier, opts.defaults);   
-      }
+    asyncLocalStorage.run(opts.defaults || {}, () => {
       done();
-    });    
+    });
   });
   next();
 }
